253d7da97b85dc97a9e5373241b75fb3e71322e5
Brian Weht <brianweht@hotmail.com>
2020-10-26 15:40:40 -0300

    ipn

--- mercadopago_ipn.php
+++ mercadopago_ipn.php
@@ -1,8 +1,7 @@
 <?php
-require('vendor/autoload.php');
-MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
 if($_GET['topic']=='payment')
 {
-   $payment = MercadoPago\Payment::find_by_id($_GET["id"]);
+   $payment = file_get_contents('https://api.mercadopago.com/v1/payments/'.$_REQUEST['id'].'?access_token=APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
+   var_dump($payment);
 }
 ?>

3bb696cdd08458a9616bab5d00d09017d70960bb
Brian Weht <brianweht@hotmail.com>
2020-10-26 15:01:12 -0300

    ipn

--- mercadopago_ipn.php
+++ mercadopago_ipn.php
@@ -4,7 +4,8 @@ MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a
 if($_GET['topic']=='payment')
 {
    $payment = MercadoPago\Payment::find_by_id($_GET["id"]);
-   $f= fopen('ipn.txt', 'wb');
-   fwrite($f, json_encode($_REQUEST));
+  // $f= fopen('ipn.txt', 'wb');
+  // fwrite($f, json_encode($_REQUEST));
+  var_dump($payment);
 }
 ?>

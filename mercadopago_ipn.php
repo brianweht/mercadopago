0fd1cc8a9c64c4c15921a111cdc505c20f485fec
Brian Weht <brianweht@hotmail.com>
2020-10-26 16:24:27 -0300

    creacion preferencia

--- mercadopago_ipn.php
+++ mercadopago_ipn.php
@@ -1,5 +1,6 @@
 <?php
 if($_GET['topic']=='payment')
+{  $f= fopen('ipn.txt', 'ab');fwrite($f, file_get_contents('php://input'));
    $payment = file_get_contents('https://api.mercadopago.com/v1/payments/'.$_REQUEST['id'].'?access_token=APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
    
    if($payment)
@@ -13,8 +14,6 @@ if($_GET['topic']=='payment')
 		  //aca iría la lógica necesaria para procesar los pagos aprobados
 	   }		   
    }
-   else
-	  http_response_code(404);
    
 }
 ?>

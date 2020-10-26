784f5c4d8dd7dfc33729a39cb9ffaf57b2d19613
Brian Weht <brianweht@hotmail.com>
2020-10-26 15:32:39 -0300

    mostrar datos en exito

--- success.php
+++ success.php
@@ -3,7 +3,15 @@
 <head>
 </head>
 <body>
-¡Listo! Se acreditó tu pago
+¡Listo! Se acreditó tu pago.
+<br />
+<h4>Información del pago</h4>
+<table>
+  <?php
+    foreach($_REQUEST as $k=>$v)
+	  echo '<tr><td><strong>'.$k.'</strong></td><td>'.$v.'</td></tr>';
+  ?>
+</table>
 <br />
 <a href="index.php">Inicio</a>
 </body>
